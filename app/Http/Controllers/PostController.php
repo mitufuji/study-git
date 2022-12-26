093471cdf8a7f9be2126da2ca7506790b45e1d7e
tess0141 <tess0141@yahoo.co.jp>
2022-12-26 13:33:04 +0900

    keijiban

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Post $post)
+    {
+        $posts = Auth::user()->posts;
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'content' => 'required',
+        ]);
+        logger('f');
+        $post = new Post();
+        $post->content = $request->input('content');
+        $post->user_id = Auth::id();        
+        // $post->user_name = Auth::name();
+        $post->save();
+        
+        
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div> 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif                                          
+    </div>
+
+    <form action="{{ route('posts.store') }}" method='post'>
+        @csrf
+        <div>
+            <label for="content">メッセージ入力</label>
+            <input type="text" class='form-control' name='content'>
+        </div>
+        <button type='submit'>メッセージ投稿</button>
+    </form>
+    @foreach($posts as $post)
+    <div>
+        <!-- <h2>{{ $post->user_name }}</h2> -->
+        <p>{{ $post->content }}</p>
+    </div>
+    @endforeach
+
+
+
+@endsection
